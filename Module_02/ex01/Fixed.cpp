f4904c8d0631c06cb1de3e77631976fd993e81fe
Maximilien Haman <mhaman@student.le-101.fr>
2021-03-24 10:28:36 +0100

    Validation Module 02

--- Module_02/ex01/Fixed.cpp
+++ Module_02/ex01/Fixed.cpp
@@ -6,14 +6,14 @@
 /*   By: mhaman <mhaman@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 13:21:04 by mhaman            #+#    #+#             */
-/*   Updated: 2021/03/23 17:33:14 by mhaman           ###   ########lyon.fr   */
+/*   Updated: 2021/03/24 10:28:19 by mhaman           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <cmath>
 
-Fixed::Fixed(/* args */){_nb = 0;}
+Fixed::Fixed(/* args */){this->_nb = 0;}
 
 Fixed::Fixed(const Fixed &c){*this = c;}
 
@@ -30,32 +30,37 @@ Fixed::Fixed(const float fl)
 {
 	float nbr = fl;
 	this->_nb = nbr;
-	nbr *= 256;
-	_nb = nbr;
+	for( int inc = 0; inc < this->_i;inc++)
+	nbr *= 2;
+	this->_nb = nbr;
 }
 
 Fixed::Fixed(const int nbr)
 {
-	_nb = nbr << _i;
+	this->_nb = nbr << this->_i;
 }
 
-int Fixed::toInt()const {return(int)(roundf(toFloat()));}
+int Fixed::toInt()const 
+{
+	int ret = roundf(toFloat());
+	return(ret);
+}
 
 std::ostream& operator<<(std::ostream& os, const Fixed& dt)
 {
-	os << (float)dt.toFloat();
+	os << dt.toFloat();
 	return os;
 }
 
 float	Fixed::toFloat(void) const
 {
-	return ((float)(_nb / (float)(1 << _i)));
+	return ((float)(this->_nb / (float)(1 << this->_i)));
 }
 
 int Fixed::getRawBits(void)const {
-	return(_nb);
+	return(this->_nb);
 }
 
 void Fixed::setRawBits(const int raw){
-	_nb = raw;
+	this->_nb = raw;
 }
